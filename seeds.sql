fac035a8ea3bd4fbdd176950703814c5044d5f7e
Kristen Cooke <kriscooke@users.noreply.github.com>
2020-04-07 13:11:32 -0700

    Create seeds for test data; improve todos default attributes

---
+++ seeds.sql
@@ -0,0 +1,8 @@
+begin;
+
+insert into todo_app.todos(task) values ('Bake bread');
+insert into todo_app.todos(task) values ('Buy toilet paper');
+insert into todo_app.todos(task) values ('Work from home');
+insert into todo_app.todos(task) values ('Take Zoom call');
+
+commit;
